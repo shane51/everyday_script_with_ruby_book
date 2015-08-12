ccfcf07bca669ba97e151e0ece60b66451ef2c60
shane51 <suse51@163.com>
2015-08-12 21:03:23 +0800

    Introduced methods and use __FILE_ and /bin/bash params to play as a main function

--- differences-version-2.rb
+++ differences-version-2.rb
@@ -3,26 +3,46 @@
 # We make no guarantees that this code is fit for any purpose. 
 # Visit http://www.pragmaticprogrammer.com/titles/bmsft for more book information.
 #---
-
-unless ARGV.length == 2
-	puts "Usage: different.rb old-inventory new-inventory"
-	exit
-end
-
-def inventory_from(filename)
-	File.open(filename).collect do | line |
-		line.downcase
+def check_usage
+	unless ARGV.length == 2
+		puts "Usage: different.rb old-inventory new-inventory"
+		exit
 	end
 end
 
-old_inventory = inventory_from(ARGV[0])
 
-new_inventory = inventory_from(ARGV[1])
+def boring?(line)
+	#puts "boring?" + line.inspect
+	line.chomp.split('/').include?('temp') or 
+	line.chomp.split('/').include?('recycler')
+end
 
-add_num = (new_inventory - old_inventory).length
-puts "The following #{add_num} files have been added:"
-puts new_inventory - old_inventory
+def inventory_from(filename)
+	inverntory = File.open(filename)
+	downcased = inverntory.collect do |line|
+		line.chomp.downcase
+	end
+	downcased.reject do |line|
+		boring?(line)
+	end
+end
 
-del_num = (old_inventory - new_inventory).length
-puts "The following #{del_num} files have been deleted:"
-puts old_inventory - new_inventory
+
+def compare_inventory_files(old_file, new_file)
+	old_inventory = inventory_from(old_file)
+
+	new_inventory = inventory_from(new_file)
+
+	add_num = (new_inventory - old_inventory).length
+	puts "The following #{add_num} files have been added:"
+	puts new_inventory - old_inventory
+
+	del_num = (old_inventory - new_inventory).length
+	puts "The following #{del_num} files have been deleted:"
+	puts old_inventory - new_inventory
+end
+
+if $0 == __FILE__
+	check_usage
+	compare_inventory_files(ARGV[0],ARGV[1])
+end
