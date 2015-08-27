07e694ceafed72459d5a603b79ec5563568795a3
shane51 <suse51@163.com>
2015-08-27 20:57:13 +0800

    Add number round for asterisks_for , and TDD drived change_count_for and extract_change_count_from method

--- TDD_churn/churn-tests.rb
+++ TDD_churn/churn-tests.rb
@@ -25,9 +25,28 @@ class ChurnTests < Test::Unit::TestCase
     assert_equal('****', asterisks_for(20))
   end
 
+  def test_asterisks_for_rounds_up_and_down
+    assert_equal('****', asterisks_for(18))
+    assert_equal('***', asterisks_for(17))
+  end
+
   def test_normal_subsystem_line_format
     assert_equal('         audit ********* (45)', subsystem_line("audit", 45))
   end
 
+  def test_subversion_log_can_have_no_changes
+    assert_equal(0, extract_change_count_from("-------------------------\
+-----------------------------\n"))
+  end
+    
+  def test_subversion_log_with_changes
+    assert_equal(2, extract_change_count_from("-------------------------\
+-----------------------------\n2531 | bem | 2005-07-01 01:1\
+1:44 -0500 (Fri, 01 Jul 2005) | 1 line\n\nrevisions up through ch 3 exer\
+cises\n---------------------------------=-------------------------------\
+-\nr2524 | bem | 2005-06-30 18:45:59 -0500 (Thu, 30 Jun 2005) | 1 line\n\
+\nresults of read-through; including renaming mistyping to snapshots\n--\
+-------------------------------------------\n")) 
+  end
 
 end
--- TDD_churn/churn.rb
+++ TDD_churn/churn.rb
@@ -3,16 +3,7 @@
 # We make no guarantees that this code is fit for any purpose. 
 # Visit http://www.pragmaticprogrammer.com/titles/bmsft for more book information.
 #---
-if $0 == __FILE__    #(1)
-  subsystem_names = ['audit', 'fulfillment', 'persistence',    #(2)
-                     'ui', 'util', 'inventory']
-  start_date = month_before(Time.now)       #(3)
 
-  puts header(start_date)                   #(4)
-  subsystem_names.each do | name |
-    puts subsystem_line(name, change_count_for(name)) #(5)  
-  end
-end
 
 def month_before(a_time)
   a_time - 28*24*60*60
@@ -28,5 +19,27 @@ def subsystem_line(subsystem_name, change_count)
 end
 
 def asterisks_for(change_count)
-	'*' * ( change_count / 5 )
+	'*' * ( change_count / 5.0 ).round
+end
+
+def change_count_for(name)
+  extract_change_count_from(svn_log(name))
+end
+def extract_change_count_from(log_txt)
+  lines = log_txt.split("\n")
+  dashed_lines = lines.find_all do  | line |
+    line.include?('----')
+  end
+  dashed_lines.length - 1
+end
+
+if $0 == __FILE__    #(1)
+  subsystem_names = ['audit', 'fulfillment', 'persistence',    #(2)
+                     'ui', 'util', 'inventory']
+  start_date = month_before(Time.now)       #(3)
+
+  puts header(start_date)                   #(4)
+  subsystem_names.each do | name |
+    puts subsystem_line(name, change_count_for(name)) #(5)  
+  end
 end
